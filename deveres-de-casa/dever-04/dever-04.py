776d9603ee0226cd9cf92b766ea449384705553a
DLS404 <111434265+DLS404@users.noreply.github.com>
2026-03-26 15:16:47 -0300

    Add files via upload
    dever 4

---
+++ deveres-de-casa/dever-04/dever-04.py
@@ -0,0 +1,15 @@
+def F(n):
+    if n == 1:
+        return 2
+    else:
+        return 2 * F(n - 1) + n**2
+
+
+def main():
+    n = 25  # coloque algum valor abaixo ou até 30 aqui
+    resultado = F(n)
+    print(f"F({n}) = {resultado}")
+
+
+if __name__ == "__main__":
+    main()
